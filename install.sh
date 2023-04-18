8a7fa3e1379c5630caec24738164392cfd843855
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2023-04-17 21:58:15 -0500

    new attempt

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-curl Ssl https://get.voltmetrix.com/voltmetrix -O /usr/local/bin/voltmetrix
+#!/bin/bash
+
+curl https://get.voltmetrix.com/voltmetrix -O /usr/local/bin/voltmetrix
 chmod +x /usr/local/bin/voltmetrix
 echo "Voltmetrix CLI installed to /usr/local/bin > refer to https://voltmetrix.com/docs to learn how to use it."
