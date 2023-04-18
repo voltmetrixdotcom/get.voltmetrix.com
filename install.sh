36eb25a0f5515b069ba437975fece3bf82d617dc
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2023-04-17 21:34:52 -0500

    first release

---
+++ install.sh
@@ -0,0 +1,13 @@
+# download voltmetrix-cli from github and install it to /usr/local/bin
+
+# download voltmetrix-cli
+
+wget https://get.voltmetrix.com/voltmetrix -O /usr/local/bin/voltmetrix
+
+# make it executable
+
+chmod +x /usr/local/bin/voltmetrix
+
+# done
+
+echo "Voltmetrix CLI installed to /usr/local/bin > refer to https://voltmetrix.com/docs to learn how to use it."
